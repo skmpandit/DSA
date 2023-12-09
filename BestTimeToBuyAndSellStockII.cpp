4778194a8f04831c00298e25e34334846067ce4b
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-12-09 15:35:27 +0530

    Best Time To Buy And Sell Stock, TC: O(N), SC: (1)

---
+++ BestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& nums) {
+    int profit=0;
+    if(nums.size() <= 1) {
+        return 0;
+    }
+    for(int i=1; i<nums.size(); i++) {
+        if(nums[i+1] > nums[i]) {
+            profit += nums[i+1] - nums[i];
+        }
+    }
+    return profit;
+}
+
+int main() {
+    vector<int> nums = {7,1,5,3,6,4};
+    int result = maxProfit(nums);
+    cout<<"The Max Profit is "<<result<<endl;
+    return 0;
+}
